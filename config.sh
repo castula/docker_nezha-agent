30c9a0a1787fe4af4abcd09f0ba6fa22fc03ed5c
castula <mail@anhsy.org>
2025-06-10 19:21:58 +0900

    update config.sh

--- config.sh
+++ config.sh
@@ -2,22 +2,22 @@
 
 cat <<EOF > /app/config.yml
 client_secret: ${CLIENT_SECRET}
+uuid: ${UUID}
+server: ${SERVER}
+tls: ${TLS}
 debug: ${DEBUG:-false}
-disable_auto_update: ${DISABLE_AUTO_UPDATE:-false}
+disable_auto_update: ${DISABLE_AUTO_UPDATE:-true}
 disable_command_execute: ${DISABLE_COMMAND_EXECUTE:-true}
-disable_force_update: ${DISABLE_FORCE_UPDATE:-false}
+disable_force_update: ${DISABLE_FORCE_UPDATE:-true}
 disable_nat: ${DISABLE_NAT:-true}
 disable_send_query: ${DISABLE_SEND_QUERY:-true}
 gpu: ${GPU:-false}
 insecure_tls: ${INSECURE_TLS:-false}
 ip_report_period: ${IP_REPORT_PERIOD:-1800}
 report_delay: ${REPORT_DELAY:-1}
-server: ${SERVER}
 skip_connection_count: ${SKIP_CONNECTION_COUNT:-true}
 skip_procs_count: ${SKIP_PROCS_COUNT:-true}
-temperature: ${TEMPERATURE:-true}
-tls: ${TLS:-true}
+temperature: ${TEMPERATURE:-false}
 use_gitee_to_upgrade: ${USE_GITEE_TO_UPGRADE:-false}
-use_ipv6_country_code: ${USE_IPV6_COUNTRY_CODE:-false}
-uuid: ${UUID}
+use_ipv6_country_code: ${USE_IPV6_COUNTRY_CODE:-true}
 EOF
